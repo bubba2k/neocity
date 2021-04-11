6f4a32cce296542147d4f65e9d118574ccec9bf4
hans <ali4bay@gmail.com>
2021-04-12 00:11:35 +0100

    initial commit

---
+++ game.js
@@ -0,0 +1,251 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+function randomIntRange(floor, ceiling)
+{
+	var random = Math.random() * (ceiling - floor) + floor;
+
+	return Math.round(random);
+}
+
+// Vector class
+class vec2
+{
+	constructor(argX = 0, argY = 0)
+	{
+		this.x = argX;
+		this.y = argY;
+	}
+
+	magnitude()
+	{
+		return Math.sqrt( Math.pow(this.x, 2) + Math.pow(this.y, 2) );
+	}
+
+	distVec(argVec)
+	{
+		var distVec = new vec2;
+
+		distVec.x = argVec.x - this.x;
+		distVec.y = argVec.y - this.y;
+
+		return distVec;
+	}
+
+	scale(argScale)
+	{
+		var result = new vec2;
+
+		result.x = this.x * argScale;
+		result.y = this.y * argScale;
+
+		return result;
+	}
+
+	add(arg)
+	{
+		var result = new vec2;
+
+		result.x = this.x + arg.x;
+		result.y = this.y + arg.y;
+
+		return result;
+	}
+
+	sub(arg)
+	{
+		var result = new vec2;
+
+		result.x = this.x - arg.x;
+		result.y = this.y - arg.y;
+
+		return result;
+	}
+
+	pow(arg)
+	{
+		var result = new vec2;
+
+		result.x = Math.pow(this.x, arg);
+		result.y = Math.pow(this.y, arg);
+
+		return result;
+	}
+}
+
+var mousePos = new vec2(0, 0);
+var freeformMode = false;
+
+// Track mouse position
+function getMousePos(canvas, evt)
+{
+	var rect = canvas.getBoundingClientRect();
+	
+	mousePos.x = evt.clientX - rect.left,
+	mousePos.y = evt.clientY - rect.top
+}
+
+canvas.addEventListener('mousemove',
+	function(evt)
+	{
+		getMousePos(canvas, evt);
+	}, false);
+
+
+let images = [];
+for(var i = 0; i < 24; i++)
+{
+	images[i] = new Image;
+
+	if(i < 10)
+	{
+		number = "0" + i.toString();
+	}
+	else
+	{
+		number = i.toString();
+	}
+
+	images[i].src = "./spheres/sphere-" + number + ".png";
+	console.log(images[i].src);
+}
+
+const gravity_constant = new vec2(0, 9.81);
+
+class Ball
+{
+	constructor(bSpawnAtCursor = true)
+	{
+		this.pos = new vec2(mousePos.x , mousePos.y + 1);
+
+		if(bSpawnAtCursor == false)
+		{
+			this.pos.x = randomIntRange(50, canvas.width - 50);
+			this.pos.y = randomIntRange(50, canvas.height - 50);
+		}
+
+		this.vel = new vec2(5, 1);
+
+		this.sprite = images[randomIntRange(0, 23)];
+
+		this.radius = randomIntRange(10, 40);
+	}
+
+	update(delta_t)
+	{
+
+		if(freeformMode == true)
+		{
+			var acceleration = new vec2(0, 0);
+		}
+		else
+		{
+			// accelerate towards cursor
+			var diff = mousePos.distVec(this.pos);
+			if(diff.magnitude() > 100)
+			{
+				acceleration = new vec2(0,0);
+			}
+			else
+			{
+				var acceleration = diff.scale(300 / diff.magnitude());
+				acceleration = acceleration.scale(1.4);
+				acceleration.y *= 1.15;
+			}
+		}
+
+		this.vel = this.vel.add(acceleration.scale(delta_t));
+
+		this.pos = this.pos.add(this.vel.scale(delta_t));
+
+		// apply gravity
+		var gravity = gravity_constant.scale(this.radius);
+		this.vel = this.vel.add(gravity.scale(delta_t));
+
+		// decelerate through drag
+		var deceleration = this.vel.scale(0.1);
+		this.vel = this.vel.sub(deceleration.scale(delta_t));
+
+		var hasCollidedX = false;
+		var hasCollidedY = false;
+		if(this.pos.x > canvas.width - this.radius)
+		{
+			this.pos.x = canvas.width - this.radius;
+
+			this.vel.x = -this.vel.x;
+
+			hasCollidedX = true;
+		}
+		else if(this.pos.x < 0 + this.radius)
+		{
+			this.pos.x = 0 + this.radius;
+
+			this.vel.x = -this.vel.x;
+
+			hasCollidedX = true;
+		}
+
+		if(this.pos.y > canvas.height - this.radius)
+		{
+			this.pos.y = canvas.height - this.radius;
+			this.vel.y = -this.vel.y;
+
+			hasCollidedY = true;
+		}
+		else if(this.pos.y < 0 + this.radius)
+		{
+			this.pos.y = 0 + this.radius;
+			this.vel.y = -this.vel.y;
+
+			hasCollidedY = true;
+		}
+
+
+		if(hasCollidedX)
+		{
+			this.vel.x *= 0.7;
+		}
+		if(hasCollidedY)
+		{
+			this.vel.y *= 0.7;
+		}
+	}
+
+	draw()
+	{
+		ctx.drawImage(this.sprite, this.pos.x - this.radius, this.pos.y - this.radius,
+					  this.radius * 2, this.radius * 2);
+	}
+};
+
+let flakes = [];
+
+for(var i = 0; i < 5; i++)
+{
+	flakes.push(new Ball(false));
+}
+
+// Mouse click callback
+canvas.addEventListener('click',
+	function(evt)
+	{
+		flakes.push(new Ball);
+		console.log("add ball");
+	}, false);
+
+
+var delay = 20;
+setInterval(function() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	flakes.forEach(function(item)
+	{
+
+		item.update(delay / 1000);
+		item.draw();
+	});
+
+
+
+
+}, delay);
